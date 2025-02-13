7d2b778db69d71d80690f6949b7d32401445822b
Seeinq <ayusfun1@gmail.com>
2025-02-13 15:45:43 -0600

    PID ends when at setpoint

--- src/main/java/frc/robot/commands/ArmCommands/ArmCommand.java
+++ src/main/java/frc/robot/commands/ArmCommands/ArmCommand.java
@@ -55,6 +55,7 @@ public class ArmCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if (m_ArmMotor.atSetpoint()) return true;
     return false;
   }
 }
