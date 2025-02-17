1e42855f78f58bdd8058ed27e5a09e79bfbcd99d
Seeinq <ayusfun1@gmail.com>
2025-02-17 12:07:40 -0600

    hopefully pid arm and elev workies

--- src/main/java/frc/robot/commands/PIDArmAndElevator.java
+++ src/main/java/frc/robot/commands/PIDArmAndElevator.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.ArmCommands.PIDArmCommand;
+import frc.robot.commands.ArmCommands.PIDArmThenHold;
 import frc.robot.commands.ElevatorCommands.PIDElevatorCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
@@ -9,7 +10,7 @@ import frc.robot.subsystems.ElevatorSubsystem;
 public class PIDArmAndElevator extends ParallelCommandGroup {
     public PIDArmAndElevator(ArmSubsystem armSubsystem, double armSetPoint, ElevatorSubsystem elevatorSubsystem, double elevatorSetPoint) {
         addCommands(
-            new PIDArmCommand(armSubsystem, armSetPoint),
+            new PIDArmThenHold(armSubsystem, armSetPoint),
             new PIDElevatorCommand(elevatorSubsystem, elevatorSetPoint)
         );
     }
